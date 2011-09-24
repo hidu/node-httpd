bf204c7c909f2df61e1c756b1844485183bd3f7d
杜伟 <duv123@gmail.com>
2011-09-24 09:01:32 +0800

    中文长度问题

--- httpd.js
+++ httpd.js
@@ -11,7 +11,7 @@ var http = require("http");
 var httpd = exports;
 
 function hand_404(req, res) {
-   var msg="the request url "+req.url+" is not on the server";
+   var msg="the request url "+decodeURI(req.url)+" is not on the server";
    hand_error(404,req,res,msg);
 }
 
@@ -19,7 +19,7 @@ function hand_error(code,req,res,msg){
  var html="<html><head><title>"+code+" Exception</title></head>";
      html+="<body><h1>"+code+" Exception</h1><p style='color:red;font-size:20px'>"+msg+"</p></body></html>";
   res.writeHead(code, { "Content-Type": "text/html"
-                     , "Content-Length": html.length
+                     , "Content-Length": strlen(html)
                      });
   res.end(html);
 }
@@ -45,6 +45,7 @@ function extname (path) {
 
 function handler_get(req,res){
   var location=url.parse(req.url);
+  location.pathname=decodeURI(location.pathname);
   sys.puts(req.url);
   var filename=config.documentRoot+location.pathname;
   path.exists(filename,function(exists){
@@ -70,19 +71,21 @@ function handler_get(req,res){
             if (err) {
                 hand_500(req, res,err.message);
             }else{
-             var body="<html><head><title>index of "+location.pathname+"</title></head><body>";
-                 body+="<h2>"+location.pathname+"</h2>";;
-                 body+="<table style='width:90%'><tr bgcolor='#c8ddf2'><th>Name</th><th>size</th><th>createTime</th></tr>";
+             var body="<html><head><meta content='text/html; charset=utf-8' http-equiv='Content-Type'>"
+                       +"<title>index of "+location.pathname+"</title></head><body>"
+                       +"<h2>"+location.pathname+"</h2>"
+                       +"<table style='width:90%'><tr bgcolor='#c8ddf2'><th>Name</th><th>size</th><th>createTime</th></tr>\n";
                  if(location.pathname!='/'){
                       files.unshift("..");
                    }
                  var p=location.pathname.replace(/\/?$/,"");
+                 
                  for(var i=0;i<files.length;i++){
-                     body+="<tr><td><a href='"+p+"/"+files[i]+"'>"+files[i]+"</a></td></tr>";
+                     body+="<tr><td><a href='"+encodeURI(p+"/"+files[i])+"'>"+files[i]+"</a></td><td></td><td></td></tr>\n";
                     }
                   body+="</body></html>";
-                 var headers = { "Content-Type": mime.lookupExtension('html')
-                                  , "Content-Length": body.length
+                 var headers = { "Content-Type": 'text/html;charset=utf-8'
+                             //   ,"Content-Length":strlen(body)
                                   };
                   res.writeHead(200, headers);
                   res.end(req.method === "HEAD" ? "" : body);   
@@ -96,7 +99,7 @@ function handler_get(req,res){
             hand_500(req, res,err.message);
           }else{
             headers = { "Content-Type": mime.lookupExtension(extname(filename))
-                      , "Content-Length": data.length
+                      , "Content-Length": strlen(data)
                       };
             res.writeHead(200, headers);
             res.end(req.method === "HEAD" ? "" : data);
@@ -124,6 +127,18 @@ for(var k in _config_argvs){
     if((v+"").length>0)
     config[_k]=v;	
 }
+
+function strlen(str){  
+    var i,len=0;  
+    for (i=0;i<str.length;i++){  
+        if (str.charCodeAt(i)>255){
+           len+=2;
+        }else{
+           len++;
+        }  
+    }  
+    return len;  
+}  
 
 server.listen(Number(config.port), config.host);
 sys.puts("Server start at:http://"+(config.host||'127.0.0.1')+":"+config.port);
